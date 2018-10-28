b1c7e7afbcc8f3c72ed8603c309068afd1a631ec
Arne Goerlitz <a-goerlitz@hotmail.de>
2018-10-28 02:59:40 +0100

    added main

--- code/neuro_krypt.py
+++ code/neuro_krypt.py
@@ -13,20 +13,22 @@ Created on Sat Oct 27 14:16:49 2018
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+#import torch.optim as optim
 from torch.autograd import Variable
+
 
 ###############################################################################
 class Krypt(nn.Module):
     def __init__(self):
         super(Krypt, self).__init__()
-        self.lin1 = nn.Linear(1000,500)
-        self.lin2 = nn.Linear(500,50)
-        self.lin3 = nn.Linear(50,10)
-        
+        self.lin1 = nn.Linear(50,1000)
+        self.lin2 = nn.Linear(1000,500)
+        self.lin3 = nn.Linear(500,50)
+
     def forward(self, x):
-        x = F.relu_(self.lin1(x))
-        x = F.sigmoid(self.lin2(x)) 
-        x = self.lin3(x)
+        x = torch.relu_(self.lin1(x))
+        x = torch.sigmoid(self.lin2(x))
+        x = torch.tanh(self.lin3(x))
         return x
 
     def num_flat_features(self, x):
@@ -34,26 +36,71 @@ class Krypt(nn.Module):
         num_features = 1
         for s in size:
             num_features *= s
-
         return num_features
 
 
+class Spy(nn.Module):
+    def __init__(self):
+        super(Spy, self).__init__()
+        self.lin1 = nn.Linear(50,500)
+        self.lin2 = nn.Linear(500,1000)
+        self.lin3 = nn.Linear(1000,50)
 
-krypt = Krypt()
-print(krypt)
+    def forward(self, x):
+        x = torch.relu_(self.lin1(x))
+        x = torch.sigmoid(self.lin2(x))
+        x = torch.tanh(self.lin3(x))
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
 
-input = Variable(torch.randn(1000,10))
-print(input)
-output = krypt(input)
-print(output)
-
- 
-# class Spy(nn.Module):
-#    def __init__(self):
-#        super(Spy,self).__init__()
-#
-# class DeKrypt(nn.Model):
+#class DeKrypt(nn.Module):
 #    def _init__(self):
-#        super(DeKrypt,self).__init__()
+#        super(DeKrypt, self).__init__()
+#        self.lin1 = nn.Linear(50,500)
+#        self.lin2 = nn.Linear(500,1000)
+#        self.lin3 = nn.Linear(1000,50)
 #
+#
+#    def forward(self, x):
+#        x = F.relu_(self.lin1(x))
+#        x = F.sigmoid(self.lin2(x))
+#        x = F.tanh(self.lin3(x))
+#        return x
+#
+#    def num_flat_features(self, x):
+#        size = x.size()[1:]
+#        num_features = 1
+#        for s in size:
+#            num_features *= s
+#        return num_features
+
+
+###############################################################################
+def main():
+
+    krypt = Krypt()
+#    dekrypt = DeKrypt()
+    spy = Spy()
+
+    input = Variable(torch.randn(50))
+    print(input)
+    output = krypt(input)
+    print(output)
+
+ #   message = dekrypt(output)
+    bugging = spy(output)
+
+#    print("Message: " + message + " | Bugging: " + bugging)
+
+
+###############################################################################
+
+if __name__ == "__main__":
+    main()
