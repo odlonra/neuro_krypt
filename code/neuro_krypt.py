d73d88ff55ec6f8a986057da06ce87d7b04fd35a
Arne Goerlitz <a-goerlitz@hotmail.de>
2018-11-03 01:07:51 +0100

    first try of backpopagation

--- code/neuro_krypt.py
+++ code/neuro_krypt.py
@@ -12,18 +12,18 @@ Created on Sat Oct 27 14:16:49 2018
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-#import torch.optim as optim
+import torch.optim as optim
 from torch.autograd import Variable
 
 
 ###############################################################################
+###############################################################################
 class Krypt(nn.Module):
     def __init__(self):
         super(Krypt, self).__init__()
-        self.lin1 = nn.Linear(50,1000)
-        self.lin2 = nn.Linear(1000,500)
-        self.lin3 = nn.Linear(500,50)
+        self.lin1 = nn.Linear(50, 1000)
+        self.lin2 = nn.Linear(1000, 500)
+        self.lin3 = nn.Linear(500, 50)
 
     def forward(self, x):
         x = torch.relu_(self.lin1(x))
@@ -37,14 +37,17 @@ class Krypt(nn.Module):
         for s in size:
             num_features *= s
         return num_features
+
+
+###############################################################################
 
 
 class Spy(nn.Module):
     def __init__(self):
         super(Spy, self).__init__()
-        self.lin1 = nn.Linear(50,500)
-        self.lin2 = nn.Linear(500,1000)
-        self.lin3 = nn.Linear(1000,50)
+        self.lin1 = nn.Linear(50, 500)
+        self.lin2 = nn.Linear(500, 1000)
+        self.lin3 = nn.Linear(1000, 50)
 
     def forward(self, x):
         x = torch.relu_(self.lin1(x))
@@ -60,47 +63,99 @@ class Spy(nn.Module):
         return num_features
 
 
-#class DeKrypt(nn.Module):
-#    def _init__(self):
-#        super(DeKrypt, self).__init__()
-#        self.lin1 = nn.Linear(50,500)
-#        self.lin2 = nn.Linear(500,1000)
-#        self.lin3 = nn.Linear(1000,50)
-#
-#
-#    def forward(self, x):
-#        x = F.relu_(self.lin1(x))
-#        x = F.sigmoid(self.lin2(x))
-#        x = F.tanh(self.lin3(x))
-#        return x
-#
-#    def num_flat_features(self, x):
-#        size = x.size()[1:]
-#        num_features = 1
-#        for s in size:
-#            num_features *= s
-#        return num_features
+###############################################################################
+class DeKrypt(nn.Module):
+    def __init__(self):
+        super(DeKrypt, self).__init__()
+        self.lin1 = nn.Linear(50, 100)
+        self.lin2 = nn.Linear(100, 500)
+        self.lin3 = nn.Linear(500, 50)
+
+    def forward(self, x):
+        x = torch.relu_(self.lin1(x))
+        x = torch.sigmoid(self.lin2(x))
+        x = torch.tanh(self.lin3(x))
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
 
 
 ###############################################################################
+###############################################################################
+def word2tensor(word):
+    for i in word:
+        
+        
+    
+    return tensor
+    
+def tensor2word():
+    
+    return word
+
+
+
+
+
+
 def main():
 
     krypt = Krypt()
-#    dekrypt = DeKrypt()
+    dekrypt = DeKrypt()
     spy = Spy()
 
-    input = Variable(torch.randn(50))
-    print(input)
-    output = krypt(input)
+    input = Variable(torch.randn(10, 50))
+    for i in range(200):
+#        print("\n\nMessage Input:\n")
+#        print(input)
+
+        output = krypt(input)
+#        print(output)
+
+        message_out = dekrypt(output)
+#        print("\n\nMessage Output:\n")
+#        print(message_out)
+
+        bugging = spy(output)
+#        print("\n\nBugging:\n")
+#        print(bugging)
+
+        ###Test
+        targ_tens = torch.zeros(10, 50)
+        targ_tens[0][0] = 1
+        targ_tens[1][17] = 1
+        targ_tens[2][13] = 1
+        targ_tens[3][4] = 1
+        target = Variable(targ_tens)
+#        print(target)
+        ###
+
+#        crit_krypt = nn.MSELoss()
+#        loss_krypt = crit_krypt(target, message_out)
+#        krypt.zero_grad()
+#        loss_krypt.backward()
+#        opti_krypt = optim.SGD(krypt.parameters(), lr=0.5)
+#        opti_krypt.step()
+
+        crit_dekrypt = nn.MSELoss()
+        loss_dekrypt = crit_dekrypt(target, message_out)
+        dekrypt.zero_grad()
+        loss_dekrypt.backward()
+        opti_dekrypt = optim.SGD(dekrypt.parameters(), lr=0.5)
+        opti_dekrypt.step()
+
+#        crit_spy = nn.MSELoss()
+#        loss_spy = crit_spy(target, message_out)
+#        spy.zero_grad()
+#        loss_spy.backward()
+#        opti_spy = optim.SGD(spy.parameters(), lr=0.5)
+#        opti_spy.step()
+
     print(output)
-
- #   message = dekrypt(output)
-    bugging = spy(output)
-
-#    print("Message: " + message + " | Bugging: " + bugging)
-
-
-###############################################################################
-
 if __name__ == "__main__":
     main()
